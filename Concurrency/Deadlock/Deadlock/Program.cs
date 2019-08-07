31324c846d0bfebc21eb9de08dd531b5e940e34d
olivetom <mauriciooliveto@gmail.com>
2019-08-07 16:26:51 -0300

    Added Concurrency Model Execution

---
+++ Concurrency/Deadlock/Deadlock/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Deadlock
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Start the delay.
+            Task task = Deadlock();
+
+            // Synchronously block, waiting for the async method to complete.
+            task.Wait();
+
+        }
+
+        static async Task Deadlock()
+        {
+            
+            // This await will capture the current context ...
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            // ... and will attempt to resume the method here in that context.
+        }
+    }
+}
